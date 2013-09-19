974d0cabdb88f901db4fe6428dc100904a947e92
James Pulec <jpulec@gmail.com>
2013-09-19 16:31:35 -0500

    Poor choices are being made

--- twnkl/settings/prod.py
+++ twnkl/settings/prod.py
@@ -2,6 +2,9 @@ from common import *
 from storages.backends.s3boto import S3BotoStorage
 import dj_database_url
 
+DEBUG= False
+TEMPLATE_DEBUG=False
+
 DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
