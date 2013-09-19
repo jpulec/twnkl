a7b99a454f749920cc6b41cc0e89ad1bb3ae1ac4
James Pulec <jpulec@gmail.com>
2013-09-19 14:31:13 -0500

    Revert changes

--- twnkl/settings/prod.py
+++ twnkl/settings/prod.py
@@ -1,10 +1,7 @@
 from common import *
 from storages.backends.s3boto import S3BotoStorage
-
-StaticRootS3BotoStorage = lambda: S3BotoStorage(location='static')
-MediaRootS3BotoStorage = lambda: S3BotoStorage(location='media')
-
 import dj_database_url
+
 DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -13,12 +10,12 @@ ALLOWED_HOSTS = ['*']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
-
-STATICFILES_STORAGE = StaticRootS3BotoStorage
-DEFAULT_FILE_STORAGE = MediaRootS3BotoStorage
 
 S3_URL = 'https://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
 STATIC_URL = MEDIA_URL = S3_URL
